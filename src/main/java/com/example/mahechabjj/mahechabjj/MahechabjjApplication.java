c8916b3f3e3f1f1f1d06928060876c5f7b785b2f
Brian Mahecha <mahebd@mail.broward.edu>
2017-07-28 17:25:50 -0400

    initial commit

---
+++ src/main/java/com/example/mahechabjj/mahechabjj/MahechabjjApplication.java
@@ -0,0 +1,12 @@
+package com.example.mahechabjj.mahechabjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahechabjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahechabjjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/mahechabjj/mahechabjj/MahechabjjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mahechabjj.mahechabjj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MahechabjjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
